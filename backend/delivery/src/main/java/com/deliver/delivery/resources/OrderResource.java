b936294afa351725e507baf13d80c7e14d8e216c
Italo Gabriel <italogabrielde@gmail.com>
2023-02-03 09:42:41 -0300

    PUT /orders/{id}/delivered

--- backend/delivery/src/main/java/com/deliver/delivery/resources/OrderResource.java
+++ backend/delivery/src/main/java/com/deliver/delivery/resources/OrderResource.java
@@ -34,4 +34,10 @@ public class OrderResource implements Serializable {
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @PutMapping(value = "/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered (@PathVariable Long id){
+        OrderDTO dto = service.setDelivered(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
 }
