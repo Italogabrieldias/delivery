a31e0cf9a39883b3647143e298fc0a476ae8f729
Italo Gabriel <italogabrielde@gmail.com>
2023-02-03 09:34:58 -0300

    post/ order

--- backend/delivery/src/main/java/com/deliver/delivery/resources/OrderResource.java
+++ backend/delivery/src/main/java/com/deliver/delivery/resources/OrderResource.java
@@ -5,11 +5,11 @@ import com.deliver.delivery.repositories.OrderRepository;
 import com.deliver.delivery.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.Serializable;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +25,13 @@ public class OrderResource implements Serializable {
     public ResponseEntity<List<OrderDTO>> findAll(){
         List<OrderDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
--- backend/delivery/src/main/java/com/deliver/delivery/services/OrderService.java
+++ backend/delivery/src/main/java/com/deliver/delivery/services/OrderService.java
@@ -2,13 +2,17 @@ package com.deliver.delivery.services;
 
 
 import com.deliver.delivery.dto.OrderDTO;
+import com.deliver.delivery.dto.ProductDTO;
 import com.deliver.delivery.entities.Order;
+import com.deliver.delivery.entities.Product;
+import com.deliver.delivery.enums.OrderStatus;
 import com.deliver.delivery.repositories.OrderRepository;
+import com.deliver.delivery.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.Serializable;
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,11 +23,24 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private ProductRepository productRepository;
     @Transactional(readOnly = true)
-    public List<OrderDTO> findAll(){
+    public List<OrderDTO> findAll() {
         List<Order> list = repository.findAll();
         return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+    Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),OrderStatus.PENDING);
+        for (ProductDTO p: dto.getProducts()){
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+       order = repository.save(order);
+        return new OrderDTO(order);
     }
 
 }
