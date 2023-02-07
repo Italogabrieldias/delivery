2d2f0355a7ebe91b4ce766ff8b28a5050d625000
Italo Gabriel <italogabrielde@gmail.com>
2023-02-06 21:12:30 -0300

    FInish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css';
 
-import Navbar from './Navbar';
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import Routers from './Routes';
 
 
@@ -10,6 +13,7 @@ function App() {
 
    
       <Routers/>
+      <ToastContainer />
   
     </>
   );
--- front-web/src/Orders/ProductsList.tsx
+++ front-web/src/Orders/ProductsList.tsx
@@ -1,23 +1,28 @@
+import { checkIsSelected } from "./helpers";
 import ProductsCard from "./ProductCard";
 import { Product } from "./types";
 type Props = {
-    products: Product[];
-}
+  products: Product[];
+  onSelectProduct: (product: Product) => void;
+  selectedProduct: Product[];
+};
 
-
-function ProductsList ({products}:Props) {
-    return (
-        <div className="orders-list-container">
-            <div className="orders-list-items">
-
-         {products.map(product =>(
-               <ProductsCard  key={product.id} product={product}/>
-         ))}
-           
-
-            </div>
-        </div>
-    );
+function ProductsList({ products, onSelectProduct, selectedProduct }: Props) {
+  return (
+    <div className="orders-list-container">
+      <div className="orders-list-items">
+        {products.map((product) => (
+          <ProductsCard
+            key={product.id}
+            product={product}
+            onSelectProduct={onSelectProduct}
+            isSelected={checkIsSelected(selectedProduct,product)}
+            
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ProductsList;
